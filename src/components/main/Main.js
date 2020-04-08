f1bf35a3973932c26a6a970fa05f357a525f2d41
sindreor <s.o_r@hotmail.com>
2020-04-08 19:51:00 +0200

    Added basic setup for desktop main component.

---
+++ src/components/main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component{
+  render(){
+    return (
+      <main>
+        <header></header>
+        <section></section>
+        <footer></footer>
+      </main>
+    );
+  }
+}
+export default Main;
---
+++ src/components/main/MainMobile.js
@@ -0,0 +1,5 @@
+class Main extends React.Component{
+  render(){
+    
+  }
+}
